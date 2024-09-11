fa4a553578dd6ebc1d3c0bab423001189154dc6e
ma-wais <codewizardsuk@gmail.com>
2024-09-11 17:14:05 +0500

    links to other charts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Uniflex from './pages/uniflex'
 import './index.css'
+import Home from './pages/Home'
+
 function App() {
 
   return (
    <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Uniflex />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/uniflex' element={<Uniflex />} />
     </Routes>
    </BrowserRouter>
   )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+
+  return (
+    <div className='flex gap-4 justify-center w-full items-center h-[100vh]'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-sm'>
+            <Link to={'/uniflex'}>Uniflix Chart</Link>
+        </button>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-sm'>
+            <a href='https://pp-costing.vercel.app/'>PP Costing</a>
+        </button>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-sm'>
+            <a href='https://mawais20212021.neocities.org/chart/excelsheet'>BOPP Bags</a>
+        </button>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-sm'>
+            <a href=''>NonWoven Sheet Print</a>
+        </button>
+    </div>
+  )
+}
+
+export default Home
