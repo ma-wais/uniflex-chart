9a194a650d9c7d5a3c5436fb2d8cb9cc6f5cb0ab
Muhammad Awais <98202831+ma-wais@users.noreply.github.com>
2024-09-16 13:30:20 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,23 +4,23 @@ import { Link } from 'react-router-dom'
 const Home = () => {
 
   return (
-    <div className='flex flex-col lg:flex-row gap-4 justify-center flex-wrap mt-4 w-full h-[120vh]'>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[70px] w-[170px] text-lg py-8 px-4 rounded-sm'>
+    <div className='flex flex-col lg:flex-row gap-4 justify-center ml-5 lg:ml-0 flex-wrap mt-4 w-full h-[120vh]'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[80px] w-[170px] text-lg py-8 px-4 rounded-sm'>
             <a href='https://pp-costing.vercel.app/'>PP Costing</a>
         </button>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[70px] w-[180px] text-lg py-8 px-4 rounded-sm'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[80px] w-[180px] text-lg py-8 px-4 rounded-sm'>
             <Link to={'/uniflex'}>Uniflex Chart</Link>
         </button>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[70px] w-[170px]text-xl py-8 px-4 rounded-sm'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[80px] w-[170px] text-xl py-8 px-4 rounded-sm'>
             <a href='https://mawais20212021.neocities.org/chart/excelsheet'>BOPP Bags</a>
         </button>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[90px] w-[200px] text-lg py-8 px-4 rounded-sm'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[100px] w-[200px] text-lg py-8 px-4 rounded-sm'>
             <a href='https://nonwoven-chart.vercel.app/'>Non Woven Sheet Print</a>
         </button>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[70px] w-[180px] text-lg py-8 px-4 rounded-sm'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[80px] w-[180px] text-lg py-8 px-4 rounded-sm'>
             <a href='https://nonwoven-bags.vercel.app/'>Non Woven Bags</a>
         </button>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[80px] w-[210px] text-lg py-8 px-4 rounded-sm'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[100px] w-[210px] text-lg py-8 px-4 rounded-sm'>
             <a href='https://mawais20212021.neocities.org/chart/bagcosting'>Non Woven Bag Costing</a>
         </button>
     </div>
