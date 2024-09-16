af4891ff3b6d0730bcb64ad5103b9898e1ecdf18
Muhammad Awais <98202831+ma-wais@users.noreply.github.com>
2024-09-16 13:14:57 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Home = () => {
 
   return (
-    <div className='flex gap-4 justify-center flex-wrap w-full items-center h-[100vh]'>
+    <div className='flex flex-col xl:flex-row gap-4 justify-center flex-wrap w-full items-center h-[100vh]'>
         <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-lg py-8 px-4 rounded-sm'>
             <a href='https://pp-costing.vercel.app/'>PP Costing</a>
         </button>
