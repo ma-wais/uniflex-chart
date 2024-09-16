08cdf7660b2157abc416dbe9b093f1b3f04f59a0
Muhammad Awais <98202831+ma-wais@users.noreply.github.com>
2024-09-16 13:23:42 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,22 +5,22 @@ const Home = () => {
 
   return (
     <div className='flex flex-col lg:flex-row gap-4 justify-center flex-wrap w-full h-[120vh]'>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-lg py-8 px-4 rounded-sm'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[50px] text-lg py-8 px-4 rounded-sm'>
             <a href='https://pp-costing.vercel.app/'>PP Costing</a>
         </button>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-lg py-8 px-4 rounded-sm'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[50px] text-lg py-8 px-4 rounded-sm'>
             <Link to={'/uniflex'}>Uniflex Chart</Link>
         </button>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl py-8 px-4 rounded-sm'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[50px] text-xl py-8 px-4 rounded-sm'>
             <a href='https://mawais20212021.neocities.org/chart/excelsheet'>BOPP Bags</a>
         </button>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-lg py-8 px-4 rounded-sm'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[50px] text-lg py-8 px-4 rounded-sm'>
             <a href='https://nonwoven-chart.vercel.app/'>Non Woven Sheet Print</a>
         </button>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-lg py-8 px-4 rounded-sm'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[50px] text-lg py-8 px-4 rounded-sm'>
             <a href='https://nonwoven-bags.vercel.app/'>Non Woven Bags</a>
         </button>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-lg py-8 px-4 rounded-sm'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold h-[50px] text-lg py-8 px-4 rounded-sm'>
             <a href='https://mawais20212021.neocities.org/chart/bagcosting'>Non Woven Bag Costing</a>
         </button>
     </div>
