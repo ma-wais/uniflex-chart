4558fc2480ae9aa0dd51e910e03c150edce3eba1
Muhammad Awais <98202831+ma-wais@users.noreply.github.com>
2024-09-16 13:07:35 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,22 +5,22 @@ const Home = () => {
 
   return (
     <div className='flex gap-4 justify-center w-full items-center h-[100vh]'>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl py-4 px-4 rounded-sm'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl py-6 px-4 rounded-sm'>
             <a href='https://pp-costing.vercel.app/'>PP Costing</a>
         </button>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl py-4 px-4 rounded-sm'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl py-6 px-4 rounded-sm'>
             <Link to={'/uniflex'}>Uniflex Chart</Link>
         </button>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl py-4 px-4 rounded-sm'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl py-6 px-4 rounded-sm'>
             <a href='https://mawais20212021.neocities.org/chart/excelsheet'>BOPP Bags</a>
         </button>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl py-4 px-4 rounded-sm'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl py-6 px-4 rounded-sm'>
             <a href='https://nonwoven-chart.vercel.app/'>NonWoven Sheet Print</a>
         </button>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl py-4 px-4 rounded-sm'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl py-6 px-4 rounded-sm'>
             <a href='https://nonwoven-bags.vercel.app/'>NonWoven Bags</a>
         </button>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl py-4 px-4 rounded-sm'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl py-6 px-4 rounded-sm'>
             <a href='https://mawais20212021.neocities.org/chart/bagcosting'>NonWoven Bag Costing</a>
         </button>
     </div>
